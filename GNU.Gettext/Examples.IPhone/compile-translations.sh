fe45db1551f595e6c52674c61febf628963d1446
rzaitov <r-zaitov@yandex.ru>
2013-06-20 14:49:38 +0400

    добавил скрипт по генерации сборок с переводами

---
+++ GNU.Gettext/Examples.IPhone/compile-translations.sh
@@ -0,0 +1,2 @@
+mono Libs/GNU.Gettext.Msgfmt.exe -v -l en-US -d ./ -L Libs/ -r enUS -o EnUS.dll Localization/En.po
+mono Libs/GNU.Gettext.Msgfmt.exe -v -l ru-RU -d ./ -L Libs/ -r ruRU -o RuRU.dll Localization/Ru.po
