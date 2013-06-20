4cdc7aaea203af72de9c59496fe4d005ac885788
rzaitov <r-zaitov@yandex.ru>
2013-06-20 14:49:18 +0400

    Добавил переводы

--- GNU.Gettext/Examples.IPhone/update-pot-file.sh
+++ GNU.Gettext/Examples.IPhone/update-pot-file.sh
@@ -2,5 +2,5 @@
 mono ../../Bin/Release/GNU.Gettext.Xgettext.exe -D ./ -o Localization/Messages.pot
 
 # merge uptodate keys to translate files
-#msgmerge --backup=none -U ../Localization/En/en.po ../Localization/Messages.pot
-#msgmerge --backup=none -U ../Localization/Ru/ru.po ../Localization/Messages.pot
+msgmerge --backup=none -U Localization/En.po Localization/Messages.pot
+msgmerge --backup=none -U Localization/Ru.po Localization/Messages.pot
